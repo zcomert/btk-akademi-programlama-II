0ee3d5cfaa07b3d6a127fa128b2513c03e98d7fd
zcomert <zafer.comert@hotmail.com>
2025-08-18 12:00:42 +0300

    Section-06 has been completed.

---
+++ Solution06/_06_05_Compare/Program.cs
@@ -0,0 +1,15 @@
+﻿// örnekler
+var a = "İstanbul";
+var b = "istanbul";
+var c = "Istanbul";
+
+System.Console.WriteLine("== vs Equals");
+System.Console.WriteLine($"{a}=={b} ? {a == b}"); // İ != i
+
+System.Console.WriteLine($"Equals({a},{b}, StringComparison.OrdinalIgnoreCase) ? " +
+$"{string.Equals(a,b, StringComparison.OrdinalIgnoreCase)}");
+
+System.Console.WriteLine($"Equals({a},{b}, StringComparison.CurrentCultureIgnoreCase) ? " +
+$"{string.Equals(a,b, StringComparison.CurrentCultureIgnoreCase)}");
+
+Console.ReadKey();
---
+++ Solution06/_06_08_StringBuilder/Program.cs
@@ -0,0 +1,54 @@
+﻿// Oturum bilgileri
+using System.Text;
+
+String userEmail = "comertzafer@gmail.com";
+String[] endpoints = { "/home", "/products/usb-c-cable", "/cart", "/checkout" };
+
+// İlk raporu üret
+string report1 = BuildSessionReport(userEmail,
+endpoints,
+sessionId: "S-1001");
+System.Console.WriteLine(report1);
+
+System.Console.WriteLine("\n\n");
+
+string report2 = BuildSessionReport("zeynep.dag@gmail.com",
+new[] { "/home", "/search" },
+ sessionId: "S-1002");
+ System.Console.WriteLine(report2);
+
+Console.ReadKey();
+
+string BuildSessionReport(string userEmail, string[] endpoints, string sessionId)
+{
+    // StringBuilder
+    var sb = new StringBuilder(capacity: 256);
+
+    // Olay ekleme: Append
+    sb.AppendLine($"User : {userEmail}");
+    sb.AppendLine($"Session Id:{sessionId}");
+    sb.Append("Visited: ");
+
+    foreach (var item in endpoints)
+    {
+        sb.Append(item).Append(", ");
+    }
+
+    // Fazla Ayıracı Temizle
+    if (endpoints.Length > 0)
+        sb.Remove(sb.Length - 2, 2);
+    sb.AppendLine();
+    sb.AppendLine($"Login Time: {DateTime.Now:yyyy-MM-dd HH:mm:ss}");
+
+    // Güvenlik
+    sb.Replace("@gmail.com", "@domain.local");
+
+    // Başlık 
+    string header = $"SESSIN LOG {DateTime.Now:yyyy-MM-dd} \n";
+    sb.Insert(0, header);
+
+    string report = sb.ToString();
+    return report;
+}
+
+Console.ReadKey();
